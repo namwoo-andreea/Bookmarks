fe42a6753136b93d4ded380b6ab03efa9a46a9b6
namwoo <garson1362@gmail.com>
2018-06-04 08:20:24 +0900

    Create LoginForm and user_login view

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import LoginForm
+
+
+def user_login(request):
+    if request.method == 'POST':
+        # Create a form with data from request.POST.
+        form = LoginForm(request.POST)
+        # Check a validity of the form
+        if form.is_valid():
+            # Assign cleaned data which created in process of is_valid()
+            cd = form.cleaned_datad
+            # Authenticate user with cleaned data, return user or None
+            user = authenticate(username=cd['username'],
+                                password=cd['password'])
+            # Check user is authenticated and is activated
+            if user is not None:
+                if user.is_active:
+                    # Login user, sets user in the current session
+                    login(request, user)
+                    return HttpResponse('Successfully Login')
+                else:
+                    return HttpResponse('Disable account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm(request.POST)
+        return render(request, 'account/login.html', {'form': form})
--- bookmarks/config/settings.py
+++ bookmarks/config/settings.py
@@ -31,7 +31,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -56,10 +55,14 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATES_DIR,
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
@@ -84,7 +86,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -104,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -117,7 +117,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
