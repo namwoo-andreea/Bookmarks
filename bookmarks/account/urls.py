8fc45da4b61585180f2c167adddf0e49b9da489d
namwoo <garson1362@gmail.com>
2018-06-06 08:38:00 +0900

    Create password_reset_form.html

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,10 +8,14 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('forgot_password', auth_views.PasswordResetView.as_view(), name='forgot-password'),
-    path('forgot_password/done/', auth_views.PasswordResetDoneView.as_view(), name='forgot-password-done'),
-    re_path('forgot_password/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/',
-            auth_views.PasswordResetConfirmView.as_view(), name='forgot-password-confirm'),
-    path('forgot_password/complete/', auth_views.PasswordResetCompleteView.as_view(),
+    path('forgot_password/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='forgot-password-done'),
+    path('forgot_password/confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='forgot-password-confirm'),
+    path('forgot_password/complete/',
+         auth_views.PasswordResetCompleteView.as_view(),
          name='forgot-password-complete'),
     # path('login/', views.user_login, name='login'),
     # path('logout/', views.user_logout, name='logout'),
