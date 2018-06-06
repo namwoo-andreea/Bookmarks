82099a27044501b69564b5a440b0eda557acff3e
namwoo <garson1362@gmail.com>
2018-06-06 17:42:30 +0900

    Change DateTimeField of date_of_birth to DateField in Profile model

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -6,5 +6,8 @@ User = get_user_model()
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    date_of_birth = models.DateTimeField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='profile/%y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return 'Profile for user {0}'.format(self.user.username)
