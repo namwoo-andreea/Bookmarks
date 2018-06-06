02e3bee46c5a1db25df78e5b54e9a046c6716c58
namwoo <garson1362@gmail.com>
2018-06-06 16:26:32 +0900

    Make Profile model

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateTimeField(blank=True, null=True)
+    photo = models.ImageField(upload_to='profile/%y/%m/%d/', blank=True)
