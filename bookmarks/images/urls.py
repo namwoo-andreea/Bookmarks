3853c277000e834bd685417df020b6bec9fe3fd8
namwoo <garson1362@gmail.com>
2018-06-11 16:56:56 +0900

    Separate js code from templates to static dir

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name='images'
+app_name = 'images'
 urlpatterns = [
     path('create/', views.image_create, name='create'),
     path('detail/<int:pk>/<slug:slug>/', views.image_detail, name='detail'),
