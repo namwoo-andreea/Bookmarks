f77140942a287c27d4d08e4c7201062ae50c3159
namwoo <garson1362@gmail.com>
2018-06-12 12:08:32 +0900

    Create Contact model as intermediary table of follower-following relationship of User

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -11,3 +11,27 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {0}'.format(self.user.username)
+
+
+class Contact(models.Model):
+    user_from = models.ForeignKey(User,
+                                  related_name='rel_from_set',
+                                  on_delete=models.CASCADE)
+    user_to = models.ForeignKey(User,
+                                related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      db_index=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return '{0} follows {1}'.format(self.user_from,
+                                        self.user_to)
+
+
+User.add_to_class('following', models.ManyToManyField('self',
+                                                      through=Contact,
+                                                      related_name='followers',
+                                                      symmetrical=False))
