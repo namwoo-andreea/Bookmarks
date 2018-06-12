e38ddf1cc4c7ca977ee6f6ce5e36a3b8904443fe
namwoo <garson1362@gmail.com>
2018-06-12 18:35:28 +0900

    Resize profile thumbnail to 56x56 in user_list and position to center

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -7,7 +7,8 @@ User = get_user_model()
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='profile/%y/%m/%d/', blank=True)
+    photo = models.ImageField(upload_to='profile/%y/%m/%d/',
+                              default='asset/default-user.jpg')
 
     def __str__(self):
         return 'Profile for user {0}'.format(self.user.username)
