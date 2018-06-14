dbbfef87086707ce6a039159d3a361e4a304e3af
namwoo <garson1362@gmail.com>
2018-06-14 17:44:54 +0900

    Add generic relationship for target

--- bookmarks/action/models.py
+++ bookmarks/action/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 User = get_user_model()
@@ -10,8 +12,17 @@ class Action(models.Model):
                              db_index=True,
                              on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
+    target_content_type = models.ForeignKey(ContentType,
+                                            related_name='target',
+                                            on_delete=models.CASCADE,
+                                            blank=True,
+                                            null=True)
+    target_object_id = models.PositiveIntegerField(db_index=True,
+                                                   blank=True,
+                                                   null=True)
+    target = GenericForeignKey('target_content_type', 'target_object_id')
     created_at = models.DateTimeField(auto_now_add=True,
                                       db_index=True)
 
     class Meta:
-        ordering = ('-created_at', )
+        ordering = ('-created_at',)
